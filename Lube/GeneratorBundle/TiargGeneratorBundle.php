3757fb78c3e4d2a15c3e501e51ded450f6e6af93
Sebastian Luberriaga <sebastian.luberriaga@tiarg.com.ar>
2016-07-02 20:57:07 -0300

    Dir Fixing

---
+++ Lube/GeneratorBundle/TiargGeneratorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lube\GeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LubeGeneratorBundle extends Bundle
+{
+}
--- Tiarg/GeneratorBundle/TiargGeneratorBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tiarg\GeneratorBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TiargGeneratorBundle extends Bundle
-{
-}
